c94471bfa3824eb91fd0eeb02df1dd83841f0809
lizmanchego <karinamanchego0416@gmail.com>
2025-04-23 18:15:00 -0500

    [ADD] Tema 2 Full

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -4,9 +4,302 @@
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
       .titulo-principal.color-acento-contenido
         .titulo-principal__numero
-          span 1
-        h1 Title
-      
+          span 2
+        h1 Políticas que favorecen los entornos saludables 
+      .row.justify-content-center.mb-4(data-aos="fade-left")
+        .col-sm-12.col-lg-1.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/1.svg', alt='')
+        .col-sm-12.col-lg-11
+          p.mb-0  Los objetivos de desarrollo del milenio (ODM): un compromiso global para promover el desarrollo humano; se entienden como Colombia, en articulación con el sistema de naciones unidas, ha venido implementando desde 2003 una agenda nacional para cumplir con los compromisos adquiridos en el marco de los objetivos de desarrollo del milenio (ODM); esta agenda, adaptada y formalizada mediante el CONPES Social 91 (2005), planteó un conjunto de propuestas y acciones para avanzar en el logro de las metas acordadas a nivel internacional, centradas en reducir la pobreza, mejorar la salud, la educación y garantizar el desarrollo humano sostenible. 
+
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-6
+          img.mb-5.d-none.d-lg-block(src='@/assets/curso/temas/t2/2.png', alt='')
+          p.mb-0.mb-cus Estos compromisos surgieron como respuesta a los desafíos globales de desarrollo social, abordados en diversas conferencias internacionales organizadas por las naciones unidas en los años 90, en septiembre del 2000, 189 estados miembros de la ONU, junto con 147 jefes de estado y de gobierno, firmaron la declaración del milenio, estableciendo un pacto mundial para erradicar la pobreza extrema y avanzar en la equidad social, bajo una visión compartida de los derechos humanos, la democracia, la equidad y la sostenibilidad ambiental. 
+        .col-sm-12.col-lg-6
+          .bg-color-11.p-4.mb-3
+            p.mb-4 Los ODM definieron ocho objetivos concretos y medibles, que buscan enfrentar problemas estructurales a nivel global: 
+            ul.lista-ul--color.mb-0
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Erradicar la pobreza extrema y el hambre.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Lograr la enseñanza primaria universal.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Promover la igualdad de género y la autonomía de la mujer.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Reducir la mortalidad infantil.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Mejorar la salud materna.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Combatir el VIH/SIDA, la malaria y otras enfermedades.   
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Garantizar la sostenibilidad del medio ambiente.   
+                li.d-flex.mb-0
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Fomentar una asociación mundial para el desarrollo.    
+
+      p.mb-5(data-aos="fade-left") A diferencia de otras agendas, el octavo objetivo establece un llamado a los países desarrollados a colaborar con los países en desarrollo, para disminuir las desigualdades internacionales y fortalecer la cooperación global. 
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Estrategias para alcanzar los objetivos del milenio en américa latina 
+
+      p.mb-5(data-aos="fade-left") Desde la mirada de américa latina, lograr los ODM requiere de esfuerzos conjuntos y sostenidos, a través de estrategias como: 
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-3.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/3.svg', alt='')
+        .col-sm-12.col-lg-9
+          ol.lista-ol--cuadro.lista-ol--separador
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 1
+              p.mb-0  Reducción de las inequidades en salud, incluyendo aquellas asociadas a pobreza, género, raza, etnia y edad. 
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 2
+              p.mb-0 Fortalecimiento de los sistemas de protección social en salud, para garantizar la cobertura universal de servicios esenciales, especialmente en las poblaciones más vulnerables. 
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 3
+              p.mb-0 Incremento del gasto público en salud y mejor distribución de los recursos, priorizando a las poblaciones con mayores necesidades. 
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 4
+              p.mb-0 Reorganización de los servicios de salud bajo el modelo de atención primaria integral, promoviendo la participación comunitaria y la equidad en el acceso. 
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 5
+              p.mb-0 Fortalecimiento de la infraestructura en salud pública, asegurando servicios adecuados y de calidad en todo el territorio.
+
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Importancia de los ODM para el desarrollo humano
+
+      p.mb-5(data-aos="fade-left") Los ODM están centrados en el ser humano, poniendo énfasis en la responsabilidad compartida de todos los países para garantizar una vida digna a todas las personas. Asimismo, promueven un enfoque basado en derechos humanos, al reconocer que sin equidad, justicia y acceso a servicios básicos, no puede existir un verdadero desarrollo, además, estos objetivos buscan crear un mundo más justo y equitativo, con legitimidad y compromiso político, impulsando la erradicación de la pobreza, la protección del ambiente, y la promoción de la paz.
+      .row.justify-content-center.mb-0(data-aos="fade-left")
+        .col-lg-5.p-0.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/4.png', alt='', style="object-fit: cover")
+        .col-lg-7.col-sm-12.align-content-center.bg-color-15
+          p.px-3.my-3 #[b Avances y desafíos actuales:] transición a los objetivos de desarrollo sostenible (ODS); si bien los ODM marcaron una agenda histórica que logró importantes avances en varios países, todavía persisten grandes desafíos relacionados con la desigualdad, el acceso a la salud, y la pobreza extrema, por ello, en 2015, la comunidad internacional adoptó los objetivos de desarrollo sostenible (ODS), ampliando y profundizando los ODM, con 17 objetivos más integrales y centrados en la sostenibilidad social, económica y ambiental. 
+          .bg-color-1.p-3.mb-3.mx-3.br-1
+            p.mb-4.fw-bold Importancia de los ODM para el desarrollo humano
+            ul.lista-ul--color.mb-0
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Persistencia de desigualdades sociales y económicas. 
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Impactos del cambio climático y degradación ambiental.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Conflictos armados y migraciones forzadas.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Falta de acceso universal a salud, educación y servicios básicos.  
+              li.d-flex.mb-0
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Necesidad de fortalecer la gobernanza, la participación ciudadana y la transparencia.
+
+
+      Separador
+      #t_2_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+        h2 2.1 Directrices nacionales que favorecen el desarrollo de las estrategias 
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-8
+          p.mb-4 En Colombia, el desarrollo de estrategias para mejorar la calidad de vida y el bienestar de la población se ha fortalecido a partir de políticas públicas intersectoriales, promovidas por el estado en alianza con diversos actores sociales, estas directrices han sido fundamentales para impulsar estrategias como escuela saludable y vivienda saludable, las cuales buscan promover entornos protectores, fortalecer capacidades individuales y colectivas, y fomentar el desarrollo humano sostenible.
+          p.mb-4 Desde el enfoque propuesto, se reconoce el papel clave de los ministerios y entidades gubernamentales en la construcción de políticas que respondan a las necesidades sociales, especialmente en el ámbito de la salud, la educación, la vivienda y el ambiente. 
+          p.mb-5  Sistema de protección social y enfoque del manejo social del riesgo, La Ley 789 de 2002, lo define como: 
+          .bloque-texto-c.bg-color-16.p-3
+            i.fas.fa-quote-right
+            h4.mb-0 El conjunto de políticas públicas orientadas a disminuir la vulnerabilidad y mejorar la calidad de vida de los colombianos, especialmente de los más desprotegidos, para obtener al menos el derecho a la salud, la pensión y el trabajo.
+        .col-sm-12.col-lg-4.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/5.png', alt='')
+
+      p.mb-4(data-aos="fade-left") Este sistema ha evolucionado desde un enfoque tradicional basado en la asistencia y previsión social, hacia un manejo social del riesgo, que reconoce la participación activa de la comunidad, el sector privado, y otros actores, para enfrentar situaciones que amenazan el bienestar y la seguridad de la población. 
+      p.mb-5(data-aos="fade-left") El manejo social del riesgo implica la prevención, mitigación y superación de situaciones adversas, incorporando estrategias de protección frente a factores como pobreza, violencia, desempleo, desastres naturales, entre otros, este enfoque se basa en tres tipos de sistemas: 
+      .row.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-4
+          .tarjeta-avatar
+            img(src='@/assets/curso/temas/t2/6.svg' alt='')
+            .tarjeta.bg-color-1.br-1
+              .p-4.mb-3
+                h5.mb-0.text-center Sistemas informales, sustentados en la solidaridad y cohesión comunitaria. 
+        .col-sm-12.col-lg-4
+          .tarjeta-avatar
+            img(src='@/assets/curso/temas/t2/7.svg' alt='')
+            .tarjeta.bg-color-1.br-1
+              .p-4.mb-3
+                h5.mb-0.text-center Sistemas de mercado, que involucran al sector privado mediante seguros y servicios.
+        .col-sm-12.col-lg-4
+          .tarjeta-avatar
+            img(src='@/assets/curso/temas/t2/8.svg' alt='')
+            .tarjeta.bg-color-1.br-1
+              .p-4.mb-3
+                h5.mb-0.text-center Sistemas públicos, impulsados por políticas, programas y regulaciones estatales. 
+
+      p.mb-4(data-aos="fade-left") Los riesgos sociales abarcan aspectos del ciclo vital (nacimiento, infancia, juventud, vejez), salud pública (enfermedades, epidemias), conflictos armados, desplazamiento, violencia, entre otros. 
+
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-8
+          h5.mb-3 Implicaciones del manejo social del riesgo:
+          ul.lista-ul--color.mb-4
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0  Promueve la participación activa de la comunidad, no limitándose a la intervención estatal. 
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0  Plantea estrategias integrales basadas en riesgos, no solo en programas aislados.
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Impulsa acciones coordinadas entre Estado, sociedad civil, sector privado y organizaciones internacionales. 
+          p.mb-0 Este enfoque se articula con los objetivos del CONPES social y las metas de la declaración del milenio, orientándose a reducir la pobreza y lograr un desarrollo humano integral. 
+        .col-sm-12.col-lg-4.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/9.png', alt='')
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Estrategia escuela saludable y su relación con el manejo social del riesgo 
+      p.mb-4(data-aos="fade-left") La estrategia escuela saludable es una herramienta clave para la gestión del riesgo, especialmente en relación a los riesgos propios del ciclo vital de niñas, niños y adolescentes, como la salud, la violencia, y los riesgos sociales, la escuela es un espacio vital donde las personas desarrollan competencias, valores y hábitos saludables.
+      p.mb-4(data-aos="fade-left") A través de políticas integradas desde los ámbitos educativo, deportivo y comunitario, se busca: 
+      .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-4.mb-cus
+          .p-4.bg-color-9.br-1.h-card-1
+            img.mb-4(src='@/assets/curso/temas/t2/10.png', alt='')
+            h5.mb-0.text-center Fortalecer la participación de la comunidad educativa. 
+        .col-sm-12.col-lg-4.mb-cus
+          .p-4.bg-color-9.br-1.h-card-1
+            img.mb-4(src='@/assets/curso/temas/t2/11.png', alt='')
+            h5.mb-0.text-center Promover ambientes protectores y saludables. 
+        .col-sm-12.col-lg-4.mb-cus
+          .p-4.bg-color-9.br-1.h-card-1
+            img.mb-4(src='@/assets/curso/temas/t2/12.png', alt='')
+            h5.mb-0.text-center Favorecer el desarrollo de capacidades para la vida, la convivencia, el autocuidado y el respeto al entorno. 
+
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Revolución educativa y desarrollo humano 
+
+      p.mb-4(data-aos="fade-left") La revolución educativa, alineada con los objetivos de desarrollo del milenio (ODM), busca garantizar el derecho a la educación de calidad, centrada en formar ciudadanos críticos, participativos y responsables. Se plantea como una respuesta integral a las necesidades de desarrollo humano, promoviendo: 
+      .row.justify-content-start.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-6
+          ol.lista-ol--cuadro.lista-ol--separador.mb-5.mt-4
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 1
+              p.mb-0 Educación de calidad con equidad.
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 2
+              p.mb-0 Acceso y permanencia en el sistema educativo. 
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 3
+              p.mb-0 Desarrollo de competencias ciudadanas y sociales.
+
+            li.d-flex
+              .lista-ol--cuadro__vineta
+                span 4
+              p.mb-0 Participación activa en la vida democrática y comunitaria. 
+          p.mb-0 El objetivo es formar personas capaces de transformar su entorno, generar procesos de desarrollo sostenible y contribuir al bienestar colectivo. 
+        .col-sm-12.col-lg-5.d-none.d-lg-block
+              img(src='@/assets/curso/temas/t2/13.svg', alt='')
+
+      p.mb-4(data-aos="fade-left") Cuando se habla de competencias ciudadanas se incluyen habilidades cognitivas, emocionales y comunicativas que permiten la participación democrática, la resolución pacífica de conflictos y la construcción de una sociedad pluralista, justa y equitativa. 
+
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+          .col-sm-12.col-lg-6.d-none.d-lg-block
+            img(src='@/assets/curso/temas/t2/14.png', alt='')
+          .col-sm-12.col-lg-6
+            .row.align-items-end.justify-content-start.mb-3
+              .col-lg-2.mb-0.d-none.d-lg-block
+                img.w-75(src='@/assets/curso/temas/t2/15.svg')
+            p.mb-2 El sistema nacional ambiental (SINA) promueve la gestión ambiental como parte del desarrollo sostenible, desde su creación, busca garantizar el equilibrio ambiental y social mediante políticas públicas que: 
+            ul.lista-ul--color.mb-4
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Conservan y restauran ecosistemas.   
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Aseguran el acceso equitativo a los recursos naturales.   
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Promueven el uso sostenible de la biodiversidad.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Impulsan el manejo responsable del agua, suelo y aire.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Articulan las acciones de política ambiental con el desarrollo regional y local.  
+            p.mb-0 Las acciones del SINA se relacionan directamente con las estrategias de vivienda saludable, al fomentar la creación de entornos habitacionales seguros, saludables y sostenibles. 
+
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Política de vivienda y desarrollo territorial 
+
+      p.mb-4(data-aos="fade-left") Desde el ministerio de vivienda, se impulsan políticas para mejorar las condiciones habitacionales, promoviendo: 
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-5
+            ul.lista-ul--color.mb-4
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Acceso a viviendas dignas y seguras.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Reducción del déficit habitacional.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Integración de criterios ambientales y sociales en la planeación territorial.  
+              li.d-flex
+                i.fas.fa-check-circle.bg-color-3
+                p.mb-0 Participación comunitaria en el diseño y gestión de los proyectos de vivienda.  
+        .col-sm-12.col-lg-3.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/16.svg', alt='')
+        .col-sm-12.col-lg-4
+          .row.justify-content-center.mb-4.flex-column.h-100
+            .col-sm-12.col-lg-2.d-none.d-lg-block.m-0.p-0
+              img(src='@/assets/curso/temas/t2/17.svg', alt='')
+            .col-sm-12.col-lg-12.mt-3
+              p.mb-0 Estas políticas se relacionan con la estrategia vivienda saludable, que busca garantizar condiciones adecuadas de salud y bienestar desde el hogar, priorizando la seguridad, la higiene, y la habitabilidad de los espacios. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
