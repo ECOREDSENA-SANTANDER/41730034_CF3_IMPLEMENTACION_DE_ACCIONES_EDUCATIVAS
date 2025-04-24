d1db2535985a7e449850e10c3662fb7c9bb50c0e
lizmanchego <karinamanchego0416@gmail.com>
2025-04-23 21:26:24 -0500

    [ADD] Tema 5 Full

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -4,9 +4,250 @@
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
       .titulo-principal.color-acento-contenido
         .titulo-principal__numero
-          span 1
-        h1 Title
+          span 5
+        h1 Integración de la estrategia de entornos saludables (EES) y la estrategia AIEPI
       
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-4
+          img.mb-5.d-none.d-lg-block(src='@/assets/curso/temas/t5/1.png', alt='')
+        .col-sm-12.col-lg-8
+          p.mb-4 La estrategia de entornos saludables (EES) se integra con AIEPI para fortalecer un enfoque preventivo y de promoción de la salud, permitiendo una intervención más efectiva en la infancia. De acuerdo con la OPS/OMS, esta integración busca: 
+          .bg-color-11.p-4.mb-5
+            ul.lista-ul--color.mb-0
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0  Identificar riesgos de manera integral, abordando las principales causas de morbilidad y mortalidad infantil en cada contexto. 
+
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0  Reducir las muertes evitables, promoviendo la transición de un modelo basado en la atención curativa hacia uno preventivo y de promoción de la salud. 
+
+                li.d-flex
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Adaptar la estrategia a los cambios epidemiológicos, priorizando intervenciones que respondan a las necesidades actuales en salud pública. 
+
+                li.d-flex.mb-0
+                  i.fas.fa-check-circle.bg-color-3
+                  p.mb-0 Asegurar el cumplimiento de compromisos internacionales, alineándose con los objetivos de desarrollo sostenible (ODS) y los planes nacionales de salud pública. 
+          p.mb-0 AIEPI y EES trabajan de manera complementaria para fortalecer la atención en salud infantil, promoviendo entornos seguros y saludables para el crecimiento y desarrollo de los niños.  
+      
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Componente comunitario de la estrategia AIEPI 
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-8
+          p.mb-4 La organización panamericana de la salud (OPS) y la organización mundial de la salud (OMS), junto con el ministerio de salud y protección social de Colombia, establecieron en 2010 la importancia del componente comunitario dentro de la estrategia AIEPI, este componente tiene un enfoque basado en la comunidad y las familias, con el propósito de promover prácticas familiares esenciales que favorezcan el crecimiento y desarrollo saludable de los niños. 
+          .row.justify-content-center.mb-4(data-aos="fade-left")
+            .col-sm-12.col-lg-2.d-none.d-lg-block
+              img.w-75(src='@/assets/curso/temas/t5/3.svg', alt='')
+            .col-sm-12.col-lg-10
+              p.mb-0 Además, se determinó que la finalidad del componente comunitario es contribuir a la equidad en salud, facilitando el acceso a servicios esenciales y la adopción de prácticas clave, especialmente en poblaciones vulnerables y en situación de exclusión social. 
+        .col-sm-12.col-lg-4.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t5/2.png', alt='')
+
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Prácticas clave del componente comunitario 
+      .row.justify-content-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-11
+          p.mb-0 Se ha demostrado que las familias que adoptan prácticas adecuadas de cuidado y salud tienen menores probabilidades de que sus hijos enfermen o fallezcan por enfermedades prevenibles. Por ello, la OPS/OMS (2010) definió como objetivo central del componente comunitario de AIEPI la incorporación de prácticas saludables en la vida cotidiana de las familias y comunidades. 
+        .col-sm-12.col-lg-1.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t5/4.svg', alt='')
+
+      .bg-color-4.mb-3(data-aos="fade-left")
+        h3.px-3.py-1 Estas prácticas están orientadas a: 
+
+      .bg-full-width.bg-fondo-14.mb-0.pt-5(data-aos="fade-left")
+        .px-4.px-md-5.pb-md-3(data-aos="fade-left")
+          .row.justify-content-center.align-items-center.mb-0
+            .col-sm-12.col-lg-12
+              p.mb-4 Desde su implementación, AIEPI ha demostrado ser una herramienta eficaz para reducir la mortalidad infantil y mejorar la calidad de vida de los niños en países de ingresos bajos y medianos. Entre los principales logros de la estrategia se destacan: 
+              SlyderF.mb-5(columnas="col-lg-4 col-xl-4")
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/5.svg', alt='')
+                    p.mb-0.text-center Garantizar el desarrollo físico y mental de los niños, promoviendo una crianza saludable. 
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/6.svg', alt='')
+                    p.mb-0.text-center Fortalecer la prevención de enfermedades comunes, como infecciones respiratorias agudas, enfermedades diarreicas y desnutrición. 
+
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/7.svg', alt='')
+                    p.mb-0.text-center Fomentar la lactancia materna exclusiva y una alimentación balanceada, para mejorar el estado nutricional infantil. 
+
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/8.svg', alt='')
+                    p.mb-0.text-center Capacitar a las familias en la identificación temprana de signos de alarma, facilitando el acceso oportuno a los servicios de salud. 
+
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/9.svg', alt='')
+                    p.mb-0.text-center Promover hábitos de higiene y saneamiento, reduciendo la incidencia de enfermedades infecciosas. 
+
+                .tarjeta.bg-fondo-18.p-3.p-md-3.p-lg-5
+                    .row.align-items-center.my-4.justify-content-center.hide-icon-sm
+                        .col-sm-12.col-md-3.col-lg-5
+                            img(src='@/assets/curso/temas/t5/10.svg', alt='')
+                    p.mb-0.text-center Incentivar la vacunación y el cumplimiento del esquema de inmunización, como medida clave de prevención. 
+
+          .row.justify-content-center.mb-5(data-aos="fade-left")
+            .col-sm-12.col-lg-10 
+              p.mb-0 El componente comunitario de AIEPI ha evolucionado con el tiempo, incorporando nuevas estrategias digitales y herramientas educativas, que permiten fortalecer la educación en salud dentro de las comunidades y garantizar una atención más integral a la infancia. 
+
+      .row.align-items-center.mb-4.titulo-icono(data-aos="fade-left")
+        .col-auto.titulo-icono-img
+          img(src='@/assets/bg/2.svg', alt='')
+        .col
+          h3.mb-0.titulo-icono-text.p-0 Prácticas clave en la estrategia AIEPI 
+
+      p.mb-4(data-aos="fade-left") En el marco de la estrategia AIEPI, se han identificado una serie de prácticas esenciales para promover el crecimiento saludable de los niños y prevenir enfermedades, para Colombia, estas prácticas han sido adaptadas de las recomendaciones de la OMS, OPS y UNICEF, añadiendo dos áreas adicionales: salud oral y prevención de la tuberculosis. 
+      p.mb-4(data-aos="fade-left") A continuación, se presentan las principales prácticas agrupadas en dos ejes fundamentales: 
+      .bg-full-width.bg-fondo-17.mb-5(data-aos="fade-left")
+        .px-4.px-md-5.py-3
+          .tarjeta.bg-color-white.py-4.px-5
+            SlyderA(tipo="b")
+              .row.justify-content-center.mb-0
+                .col-sm-12.col-lg-8
+                  h4.mb-3 1. Crecimiento y desarrollo:
+                  p.mb-4 Estas prácticas están orientadas a garantizar una nutrición adecuada y un desarrollo integral en los niños, a continuación de describen las siguientes.
+                  ul.lista-ul--color.mb-0
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Fomentar la lactancia materna exclusiva durante los primeros seis meses de vida.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Introducir alimentación complementaria a partir de los seis meses, asegurando cantidad, calidad y continuidad de la lactancia materna hasta los dos años o más.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Consumo de micronutrientes esenciales, como vitamina A y hierro, a través de la alimentación o mediante suplementos.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Estimulación temprana, afecto y desarrollo socioemocional, creando un entorno seguro y estimulante para el crecimiento del niño.
+                .col-sm-12.col-lg-4.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/t5/11.png', alt='')
+
+              .row.justify-content-center.mb-0
+                .col-sm-12.col-lg-8
+                  h4.mb-3 2. Prevención de enfermedades:
+                  p.mb-4 Estas prácticas buscan reducir la incidencia de enfermedades prevenibles mediante medidas de promoción de la salud:
+                  ul.lista-ul--color.mb-0
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Vacunación oportuna de los niños según el esquema nacional establecido.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Prácticas adecuadas de higiene y saneamiento básico, incluyendo lavado de manos, acceso a agua potable y disposición adecuada de desechos.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Prevención de malaria y dengue en zonas endémicas mediante control de vectores y uso de medidas de protección.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Prevención del VIH/SIDA, promoviendo medidas de comportamiento preventivo y acceso a tratamiento para personas afectadas.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Salud oral infantil, con visitas regulares al odontólogo cada seis meses para la prevención de caries y enfermedades bucales.
+                  p.mb-4 Protección contra la tuberculosis (TBC) en niños expuestos, asegurando diagnóstico temprano y acceso a tratamiento preventivo.
+                .col-sm-12.col-lg-4.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/t5/12.png', alt='')
+
+
+              .row.justify-content-center.mb-0
+                .col-sm-12.col-lg-8
+                  h4.mb-3 3. Cuidados en el hogar:
+                  p.mb-4 Las siguientes prácticas ayudan a garantizar un entorno seguro y saludable para el niño en su hogar:
+                  ul.lista-ul--color.mb-0
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Alimentación del niño enfermo en casa, asegurando el consumo adecuado de líquidos, con énfasis en la leche materna para mantener una buena nutrición e hidratación.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Tratamiento adecuado en el hogar, asegurando el cumplimiento de los cuidados básicos y el uso correcto de los medicamentos prescritos.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Buen trato y protección infantil, promoviendo un ambiente libre de maltrato, abuso, negligencia o explotación infantil.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Participación activa del padre y otros miembros de la familia en el cuidado del niño, la salud de la mujer gestante y la planificación familiar.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Prevención de accidentes en el hogar, asegurando un entorno seguro con medidas de protección contra lesiones, caídas, intoxicaciones y otros riesgos domésticos.
+                .col-sm-12.col-lg-4.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/t5/13.png', alt='Madre cuidando a un niño enfermo en el hogar')
+
+              .row.justify-content-center.mb-0
+                .col-sm-12.col-lg-8
+                  h4.mb-3 4. Búsqueda oportuna de atención médica:
+                  p.mb-4 Es fundamental reconocer signos de alerta y acudir a los servicios de salud a tiempo para evitar complicaciones:
+                  ul.lista-ul--color.mb-0
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Identificación temprana de signos de alarma, como fiebre alta, dificultad respiratoria, diarrea persistente o desnutrición.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Seguir las recomendaciones del personal de salud, asegurando la adherencia a tratamientos y controles médicos.
+                    li.d-flex
+                      i.fas.fa-check-circle.bg-color-3
+                      p.mb-0 Atención integral a la mujer gestante, incluyendo controles prenatales, vacunación (como la antitetánica), asesoramiento nutricional y apoyo durante el parto, posparto y lactancia.
+                .col-sm-12.col-lg-4.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/t5/14.png', alt='Consulta médica con madre e hijo')
+
+      .tarjeta.bg-fondo-16.p-3.mb-5(data-aos="fade-left")
+        .row.justify-content-around.align-items-center
+          .col-3.col-sm-2.col-lg-1.d-none.d-lg-block
+            img(src="@/assets/curso/temas/t4/3.png")
+          .col
+            .row.justify-content-between.align-items-center.text-white
+              .col.mb-3.mb-sm-0
+                h4.mb-1 Para fortalecer el tema anteriormente plasmado, se presenta el siguiente video: 
+                p.mb-0 https://www.youtube.com/watch?v=qIhY7mFNvpw
+              .col-sm-auto
+                a.boton.color-acento-botones.text-sistema(href="https://www.youtube.com/watch?v=qIhY7mFNvpw" target="_blank")
+                  span Ver video
+                  i.fas.fa-link
+
+      .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+        .col-sm-12.col-lg-5.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t5/15.png', alt='')
+        .col-sm-12.col-lg-7
+          .row.justify-content-center.mb-0.flex-column.h-100
+            .col-sm-12.col-lg-2.d-none.d-lg-block.m-0.p-0.mb-2
+              img.w-50(src='@/assets/curso/temas/t5/16.svg', alt='')
+            .col-sm-12.col-lg-12
+              p.mb-0 La estrategia de atención integrada a las enfermedades prevalentes de la infancia (AIEPI) ha demostrado ser una herramienta fundamental en la reducción de la morbilidad y mortalidad infantil, promoviendo un enfoque integral que combina la atención en los servicios de salud con la participación activa de las familias y comunidades, a través de sus componentes institucional, clínico y comunitario, AIEPI fortalece la capacidad de los sistemas de salud, mejora la calidad de la atención médica y fomenta prácticas clave en el hogar que garantizan el bienestar infantil. 
+
+      .row.justify-content-center.align-items-start.mb-0(data-aos="fade-left")
+        .col-sm-12.col-lg-6.mb-cus
+          .bg-color-14.br-1.px-4.py-4.mb-0
+              p.mb-0.py-2 La integración de AIEPI con estrategias como entornos saludables (EES) y la promoción de prácticas esenciales en nutrición, prevención de enfermedades, cuidados en el hogar y búsqueda oportuna de atención médica, han permitido mejorar la equidad en salud, especialmente en poblaciones vulnerables, a medida que evolucionan los desafíos en salud pública, la estrategia sigue adaptándose mediante la incorporación de tecnología, educación en salud y acciones comunitarias, asegurando un impacto sostenible en la infancia. 
+        .col-sm-12.col-lg-3.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t5/17.svg', alt='')
+        .col-sm-12.col-lg-3
+          .row.justify-content-center.mb-4.flex-column.h-100
+            .col-sm-12.col-lg-2.d-none.d-lg-block.m-0.p-0
+              img.w-75(src='@/assets/curso/temas/t5/18.svg', alt='')
+            .col-sm-12.col-lg-12.mt-1
+              p.mb-0 El compromiso con la prevención, atención temprana y fortalecimiento del entorno familiar sigue siendo clave para garantizar que todos los niños y niñas crezcan en condiciones óptimas, con acceso a una atención de calidad y un desarrollo saludable desde los primeros años de vida. 
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
